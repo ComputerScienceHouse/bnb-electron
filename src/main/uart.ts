85126c306802dbe0321e3e7cb0a120ec97e40e1a
sahil <shp2382@g.rit.edu>
2025-09-23 22:56:41 -0400

    testing uart.ts

--- src/main/uart.ts
+++ src/main/uart.ts
@@ -1,3 +1,17 @@
 import { SerialPort } from "serialport";
 
-const port = new SerialPort({ path: "COM3", baudRate: 115200 });
+const UART_PORT_1 = 'dev/tty0'
+const UART_PORT_2 = 'dev/ttyAMA1'
+
+const port = new SerialPort({ path: UART_PORT_1, baudRate: 115200 });
+
+port.write('Hello from Electron!', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+});
+
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
